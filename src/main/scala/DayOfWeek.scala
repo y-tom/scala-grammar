e8789388471d3a1fc4cfafa12db8c3ae4fae3fb6
yukako <114702943+y-tom@users.noreply.github.com>
2026-01-18 09:54:48 +0900

    ケースクラスと同値性

---
+++ src/main/scala/DayOfWeek.scala
@@ -0,0 +1,22 @@
+sealed abstract class DayOfWeek
+case object Sunday extends DayOfWeek
+case object Monday extends DayOfWeek
+case object Tuesday extends DayOfWeek
+case object Wednesday extends DayOfWeek
+case object Thursday extends DayOfWeek
+case object Friday extends DayOfWeek
+case object Saturday extends DayOfWeek
+
+object DayOfWeek {
+  def toNum(day: DayOfWeek): Int = {
+    day match {
+      case Sunday => 1
+      case Monday => 2
+      case Tuesday => 3
+      case Wednesday => 4
+      case Thursday => 5
+      case Friday => 6
+
+    }
+  }
+}
