4dc6639caa478f3956d0db9caf971c77db99fa44
yukako <114702943+y-tom@users.noreply.github.com>
2026-01-18 11:51:55 +0900

    Scalaで解く最短経路

---
+++ src/main/scala/Graph.scala
@@ -0,0 +1,9 @@
+object Graph {
+  val matrix: Array[Array[Int]] = Array(
+    Array(0, 1, 1, 0, 0),
+    Array(1, 0, 0, 1, 0),
+    Array(1, 0, 0, 1, 1),
+    Array(0, 1, 1, 0, 1),
+    Array(0, 0, 1, 1, 0)
+  )
+}
